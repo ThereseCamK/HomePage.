b86c231dba4425f9db2f4f3fbbe775bf41516492
Therese C Nordnes <69575527+ThereseCamK@users.noreply.github.com>
2022-05-04 14:57:55 +0200

    Add files via upload

--- model.js
+++ model.js
@@ -1,4 +1,25 @@
 const model ={
-    currentpage: '',
+    currentpage: 'home',
     view:'',
+   
+
+    prosjects:[
+        {
+            name:'KrokkertRegler',
+            info: 'Noe jeg laget for en familie turnering hvor det ofte blir kranglig om reglene',
+            linkToPage: 'https://theresecamk.github.io/CroquetRuels/index.html',
+            
+        },
+        {
+            name: 'Timeplan for jobben',
+            info: 'Laget en timeplan for faste avtaler på jobb, så vi enkelt kunne se hvor andre hva til enhver tid',
+            linkToPage: 'https://theresecamk.github.io/GETacademyTimePlan/',
+        },
+        {
+            name: 'Prototype i modul 2',
+            info: 'Laget denne for kune i modul 2, dette var ett samarbeidsprosjekt på en prototype som vi brukte 7 uker på og det var så langt vi kom. lært masse på ett slikt prosjket .  ',
+            linkToPage: 'https://thereseck.github.io/therese-og-even/therese-og-even-master/prototype/index.html',
+        }
+
+    ]
 }
