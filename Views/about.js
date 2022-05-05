b8cb669a4cd51f0072ed6a259911e22f919a12e3
Therese C Nordnes <69575527+ThereseCamK@users.noreply.github.com>
2022-05-05 09:50:30 +0200

    Add files via upload

--- Views/about.js
+++ Views/about.js
@@ -5,7 +5,7 @@ function aboutView(){
 
     <div class="aboutTxt">    <p> 
     <b>Tillit er viktig for meg, så for at du skal få tillit til meg så føler jeg at det er best å spille med åpne kort fra start !</b><br><br>
-                    Jeg er 31 år og har akkurat gjennomført det største veiskille som har vært til nå i livet mitt. Nemlig stort karriereskifte, det er fortsatt veldig surrealistisk og uvirkelig.
+                    Jeg er 32 år og har akkurat gjennomført det største veiskille som har vært til nå i livet mitt. Nemlig stort karriereskifte, det er fortsatt veldig surrealistisk og uvirkelig.
 Det har seg nemlig slik at jeg valgte restaurant og matfag på videregående veldig uvitende om hva jeg EGENTLIG ville jobbe med resten av livet. Altså fagbrevet henger fortsatt på veggen hjemme, men føler egentlig det ikke hører til meg.
 <br><br>Etter en del år med deltidsjobber her og der så var det vanskelig å finne noe fast og seriøst. Dermed så ville jeg begynne på nytt på studiene og igjen så visste jeg egentlig hva jeg VILLE bli og tok mange runder med meg selv. Endte da med å ta generell studiekompetanse for å starte på bachelor i økonomi og administrasjon med ett mål om å bli regnskapsfører.
 Fullførte kompetansen og begynte på BI på nettstudier samtidig som jeg gikk gravid med med barn nummer 2. Hadde en plan om å ta det på nett i permisjonstiden. <br><br> Mine planer gikk ikke helt som jeg håpet fordi jeg fikk blodpropp i både i lysken og lunger under svangerskapet. Dette gjorde at jeg har fått noen utfordringer helsemessig og gått gjennom litt psykisk slit rundt dette. Prøvde hardt å fortsette med studiene etter dette, men det var ikke lett. I tillegg så ville ikke nav at jeg skulle studere mens jeg var sykemeldt så var også på en måte nødt til å avslutte uansett.
@@ -19,7 +19,7 @@ Første tanke var:<b> HVA ER DET? , JEG KAN JO IKKE NOE OM DATA og DET ER JO IKK
 Etter litt overtalelse fikk hun meg til å google og se hva det var for noe, og dermed var jeg hektet! En uke etter så fikk jeg tilbud om å gå på GET Academy her i Larvik for å lære meg programmering i form av javascript, HTML/CSS og C# var 3 moduler på 2 mnd hver og jeg stortrives virkelig, samtidig så jobbet vi med nøkkelkompetanser og jeg ble tryggere på meg selv.
 Når disse 6 mnd var over så fikk jeg jaggu tilbud om å få praksisplass der som lærer i IT og nøkkelkompetanse. året etter var jeg ferdig og fikk fast jobb så jeg er her enda og lærer enda mer nytt.
 <br> <br>
-Jeg Jakter stadig på nye utfordringer og mer kompetanse innenfor faget, så nå jakter jeg på en jobb som ren utvikler, frontend er det jeg kanskje mestrer best, men lyst til å lære med backend også.
+Jeg jakter stadig på nye utfordringer og mer kompetanse innenfor faget, så nå er jeg på utskikk etter en jobb som ren utvikler, frontend er det jeg kanskje mestrer best, men lyst til å lære meg backend også.
     </p></div>
 
     <div class="linkToSome"><a href="https://www.facebook.com/therese.nordnes.58/" class="fa fa-facebook"></a></div>
