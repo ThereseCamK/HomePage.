32c93ddc8feddfde7c74fa0f987035daa1f85929
Therese C Nordnes <69575527+ThereseCamK@users.noreply.github.com>
2022-05-05 08:35:10 +0200

    Add files via upload

--- Views/about.js
+++ Views/about.js
@@ -3,14 +3,24 @@ function aboutView(){
     
     <div class="aboutHeadLine"> <h1>Litt om  meg og min reise til den jeg er i dag.</h1></div>
 
-    <div class="aboutTxt"><p>Jeg er ei dame som jobber på GET Academy som lærer i programmering og med nøkkelkompetanser. <br>
-    At jeg sitter her som IT-Utvikler hadde ingen trudd for noen år siden, personlig så jeg på alle som jobbet med dette som de smarte, og det var
-    jo ikke meg: "JEG ER JO IKKE SMART!"- det var mine tanker. og hadde ingen fantasi om at jeg kunne klare noe av dette.
-    MEN etter sykdom og måtte omskoleres fra kokke-yrket så var det ei som koblet interessene mine med at dette kunne være noe for meg, -"ALDRI VERDEN, JEG KAN JO INGENTING OM DATA!"-.
-    Lovte å gi det ett forsøk, og fra første stund så har det vært full klaff, det har også vært tøft og utfordrende å lære programmering.
-    Nå 1,5 år senere har jeg laget denne siden, og det er jeg rimelig stolt av!.
-    MEN jeg er ikke ferdig å lære og vil gjerne hjelpe deg med små nettsider eller noe lignende for å få mer kompetanse!
-    Kontakt meg gjerne for en uforpliktende prat!</p></div>
+    <div class="aboutTxt">    <p>  Hei! Det å skrive om meg  er nok ikke min sterkeste side men må jo gi det et forsøk eller to. <br>
+    <b>Tillit er viktig for meg, så for at du skal få tillit til meg så føler jeg at det er best å spille med åpne kort fra start !</b><br><br>
+                    Jeg er 31 år og har akkurat gjennomført det største veiskille som har vært til nå i livet mitt. Nemlig stort karriereskifte, det er fortsatt veldig surrealistisk og uvirkelig.
+Det har seg nemlig slik at jeg valgte restaurant og matfag på videregående veldig uvitende om hva jeg EGENTLIG ville jobbe med resten av livet. Altså fagbrevet henger fortsatt på veggen hjemme, men føler egentlig det ikke hører til meg.
+<br><br>Etter en del år med deltidsjobber her og der så var det vanskelig å finne noe fast og seriøst. Dermed så ville jeg begynne på nytt på studiene og igjen så visste jeg egentlig hva jeg VILLE bli og tok mange runder med meg selv. Endte da med å ta generell studiekompetanse for å starte på bachelor i økonomi og administrasjon med ett mål om å bli regnskapsfører.
+Fullførte kompetansen og begynte på BI på nettstudier samtidig som jeg gikk gravid med med barn nummer 2. Hadde en plan om å ta det på nett i permisjonstiden. <br><br> Mine planer gikk ikke helt som jeg håpet fordi jeg fikk blodpropp i både i lysken og lunger under svangerskapet. Dette gjorde at jeg har fått noen utfordringer helsemessig og gått gjennom litt psykisk slit rundt dette. Prøvde hardt å fortsette med studiene etter dette, men det var ikke lett. I tillegg så ville ikke nav at jeg skulle studere mens jeg var sykemeldt så var også på en måte nødt til å avslutte uansett.
+<br>
+Hva skulle jeg gjøre nå? Uføre som de egentlig ønsket siden det stod i papirene at jeg ikke klarte å jobbe mer enn 20 % var ikke et alternativ for meg en gang!
+<br>
+Fikk da plass på arbeidsrettet tiltak for å finne ut hva jeg vil og kan jobbe med.<br>
+Bare noen få dager med samtaler med veileder så foreslo henne programmering ut fra mine svar på hvordan jeg kobler av: nemlig med sudoku, og andre aktiviteter med hjernetrim.
+Første tanke var:<b> HVA ER DET? , JEG KAN JO IKKE NOE OM DATA og DET ER JO IKKE NOE FOR MEG, JEG ER IKKE SMART NOK TIL DET!</b>
+<br><br>
+Etter litt overtalelse fikk hun meg til å google og se hva det var for noe, og dermed var jeg hektet! En uke etter så fikk jeg tilbud om å gå på GET Academy her i Larvik for å lære meg programmering i form av javascript, HTML/CSS og C# var 3 moduler på 2 mnd hver og jeg stortrives virkelig, samtidig så jobbet vi med nøkkelkompetanser og jeg ble tryggere på meg selv.
+Når disse 6 mnd var over så fikk jeg jaggu tilbud om å få praksisplass der som lærer i IT og nøkkelkompetanse. året etter var jeg ferdig og fikk fast jobb så jeg er her enda og lærer enda mer nytt.
+<br> <br>
+Jeg Jakter stadig på nye utfordringer og mer kompetanse innenfor faget, så nå jakter jeg på en jobb som ren utvikler, frontend er det jeg kanskje mestrer best, men lyst til å lære med backend også.
+    </p></div>
 
     <div class="linkToSome"><a href="https://www.facebook.com/therese.nordnes.58/" class="fa fa-facebook"></a></div>
     <div class="linkToSome"><a href="https://www.linkedin.com/in/therese-camilla-kj%C3%A6r-3a223420b/" class="fa fa-linkedin"></a></div>
--- Views/cv.js
+++ Views/cv.js
@@ -1,4 +1,65 @@
 function cvView(){
-    model.view = `<p> HER kommer CV</p>`;
+    model.view = ` <div class="mainContentCV"> <h4>CV</h4>
+    ${mainCv()}
+     <h4>Nøkkelkvalifikasjoner</h4>
+     ${getKeyQualifications()}
+     <hr>
+     <h4>Utdanning & Arbeidserfaring</h4>
+         ${getEducationTable()}
+         ${getWorkExperienceTable()}
+         <hr>
+     <h4>Annen Kompetanse</h4>
+  
+     <p>    HMS- Verneombud <br>IK-Matkurs<br>IMO 60 Kurs 2012<br>Salgskurs 2012</p>
+      </div>
+`;
     show()
+}
+
+function mainCv(){
+    let cv = model.cvInfo;
+let html = ` <table class="mainCV">
+                 <tr><td>Navn:</td><td>${cv.name}</td></tr>
+                 <tr><td>Adresse:</td><td>${cv.address} </td></tr>
+                 <tr><td>Telefon:</td><td>${cv.phone} </td></tr>
+                 <tr><td>Epost:</td><td>${cv.email} </td></tr>
+                 <tr><td>Født:</td><td>${cv.born} </td></tr>
+                 <tr><td>Sivilstand:</td><td>${cv.maritalStatus} </td></tr>
+                 <tr><td>Språk:</td><td>${cv.language} </td></tr>
+                 <tr><td>Interesser:</td><td>${cv.hobbys} </td></tr>
+           </table>`;
+return html;
+}
+
+function getKeyQualifications(){
+let html = '<ul class="qualifications">';
+for(let i =0; i< model.keyQualifications.length; i++){
+    html += `<li>${model.keyQualifications[i]}</li>`
+}
+html += '</li>';
+
+    return html;
+}
+
+function getEducationTable(){
+let html = '<table class="educationCV"> <tr><th>År</th><th>Utdannings sted</th></tr>'
+    for (let i = 0; i < model.education.length; i++){
+        html += `<tr><td><b>${model.education[i].year}</b></td>
+                        <td><b>${model.education[i].place}</b><br><p>${model.education[i].description}</p><br></td>
+                    </tr>`
+    }
+    html += '</table>';
+    return html;
+
+}
+
+function getWorkExperienceTable(){
+let html = '<table class="workExperienceVC" <tr><th>År</th><th>Arbeidsted</th></tr>'
+    for(let i = 0; i <model.workExperience.length; i++){
+        html += `<tr><td><b>${model.workExperience[i].year}</b></td>
+                        <td><b>${model.workExperience[i].place}</b><br><p>${model.workExperience[i].title}<br>${model.workExperience[i].description}</p></td>
+                    </tr>`
+    }
+    html += '</table>'
+    return html;
 }
